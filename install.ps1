cc8113f76ff7b0fa7f8b09d9b25a5455500f9a31
youvita <chanyouvita@gmail.com>
2025-12-20 12:25:17 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -30,7 +30,7 @@ if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
         Set-ExecutionPolicy Bypass -Scope Process -Force
         [System.Net.ServicePointManager]::SecurityProtocol = 
 [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-        Invoke-Expression ((New-Object 
+        Invoke-Expression ((New-Object) 
 System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     }
     Log "Installing Git via Chocolatey..."
